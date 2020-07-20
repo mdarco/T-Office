2a1785df63c8fab02b4b746ccbb7899c0541a964
Darko Milutinovic <dmilutinovic@gmail.com>
2020-07-20 23:49:57 +0200

    added T-Office.ApiCore

---
+++ T-Office.ApiCore/Controllers/RegLicenseReaderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using T_Office.ApiCore.Hubs;
+
+namespace T_Office.ApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegLicenseReaderController : ControllerBase
+    {
+        private readonly IHubContext<TOfficeHub> _hub;
+
+        public RegLicenseReaderController(IHubContext<TOfficeHub> hub)
+        {
+            this._hub = hub;
+        }
+
+        // TODO:
+    }
+}
---
+++ T-Office.ApiCore/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using T_Office.ApiCore.Hubs;
+
+namespace T_Office.ApiCore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options => {
+                options.AddPolicy("CorsPolicy", builder => {
+                    builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+            });
+
+            services.AddSignalR();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // important: this should go before UseAuthorization() and/or UseEndpoints() methods
+            app.UseCors("CorsPolicy");
+
+            // app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<TOfficeHub>("/tofficehub");
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+            });
+        }
+    }
+}

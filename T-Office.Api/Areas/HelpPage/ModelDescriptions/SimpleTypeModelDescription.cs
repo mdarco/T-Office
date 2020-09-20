4e0fd76c8250c719849177aaa1224c68ee9f65b3
Darko Milutinovic <dmilutinovic@gmail.com>
2020-09-20 20:47:47 +0200

    removing old Api

--- T-Office.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace T_Office.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- T-Office.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="T_Office.Api.WebApiApplication" Language="C#" %>

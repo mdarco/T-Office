ea0a5c7b5bc97ea3e7f37277a6bd3b352c2a9669
Darko Milutinovic <dmilutinovic@gmail.com>
2020-10-11 23:41:05 +0200

    reg license reader controller fix

--- T-Office.ApiCore/Controllers/RegLicenseReaderController.cs
+++ T-Office.ApiCore/Controllers/RegLicenseReaderController.cs
@@ -24,8 +24,17 @@ namespace T_Office.ApiCore.Controllers
         // and issue the smart card reader read data command
         [Route("read/{wsConnectionId}")]
         [HttpGet]
-        public IActionResult ReadSmartCard(string wsConnectionId) {
+        public IActionResult ReadSmartCardData(string wsConnectionId) {
             _hub.Clients.Client(wsConnectionId).SendAsync("readSmartCardData");
+            return Ok();
+        }
+
+        [Route("response/{wsConnectionId}")]
+        [HttpPost]
+        public IActionResult SmartCardReaderResponse(string wsConnectionId, string regLicenseData)
+        {
+            // TODO: save smart card reader data to in-memory DB with 'wsConnectionId' as a key
+            // WebApp will poll it for the result and once it obtains it, it will delete the key and data from the in-memory DB
             return Ok();
         }
     }

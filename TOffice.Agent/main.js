08bb94a41b438a3a8d8c1fb79464af8e807bff68
Darko Milutinovic <dmilutinovic@gmail.com>
2020-11-04 15:28:29 +0100

    tray support 01

--- TOffice.Agent/main.js
+++ TOffice.Agent/main.js
@@ -1,4 +1,4 @@
-﻿const { app, BrowserWindow, ipcMain } = require('electron');
+﻿const { app, BrowserWindow, Tray, Menu, ipcMain } = require('electron');
 const url = require('url');
 const path = require('path');
 const macaddress = require('macaddress');
@@ -6,6 +6,7 @@ const macaddress = require('macaddress');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win = null;
+let tray = null;
 
 function createWindow() {
     // Create the browser window
@@ -39,10 +40,33 @@ function createWindow() {
     });
 }
 
+function createTray() {
+    tray = new Tray(path.join(__dirname, '/assets/beetle.png'));
+
+    if (process.platform === 'win32') {
+        tray.on('click', tray.popUpContextMenu);
+    }
+    const menu = Menu.buildFromTemplate([
+        {
+            label: 'Quit',
+            click() { app.quit(); }
+        }
+    ]);
+
+    tray.setToolTip('T-Office');
+    tray.setContextMenu(menu);
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+    createWindow();
+    createTray();
+
+    // hide dock icon
+    // if (app.dock) app.dock.hide();
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {

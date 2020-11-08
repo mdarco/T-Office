9ec4df4eb1f6500c22137929a95e6daf1977bd2f
Darko Milutinovic <dmilutinovic@gmail.com>
2020-11-08 12:35:40 +0100

    add agent env production

--- TOffice.Agent/app/src/environments/environment.prod.ts
+++ TOffice.Agent/app/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000'
+  apiUrl: 'http://localhost:5000/api',
+  signalrUrl: 'http://localhost:5000/tofficehub',
+  webAppUrl: 'http://localhost:5555/',
+  regLicenseReaderPath: 'RegLicenseReader.exe'
 };

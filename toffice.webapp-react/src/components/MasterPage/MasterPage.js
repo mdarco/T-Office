f121b04e8025a71c62a125b4191bda4cc767afa4
Darko Milutinovic <dmilutinovic@gmail.com>
2021-02-11 23:19:08 +0100

    not found page image

--- toffice.webapp-react/src/components/MasterPage/MasterPage.js
+++ toffice.webapp-react/src/components/MasterPage/MasterPage.js
@@ -391,7 +391,7 @@ function MasterPage() {
 						className="flex-1 relative z-0 overflow-y-auto focus:outline-none"
 						tabIndex="0"
 					>
-						<div className="py-6">
+						<div>
 							{/* <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 							<h1 className="text-2xl font-semibold text-gray-900">
 								Dashboard
--- toffice.webapp-react/src/components/NotFound/NotFound.js
+++ toffice.webapp-react/src/components/NotFound/NotFound.js
@@ -3,7 +3,13 @@ import * as React from 'react';
 // import './NotFound.css';
 
 function NotFound() {
-	return <div>NotFound</div>;
+	return (
+		<div className="flex flex-col justify-center items-center h-screen">
+			<div>
+				<img src="img/404.png" alt="Page not found" />
+			</div>
+		</div>
+	);
 }
 
 export default NotFound;

0e5ccee2b3ca749a8e4f4201036683db28d87872
Darko Milutinovic <dmilutinovic@gmail.com>
2021-02-14 13:34:25 +0100

    NoData component image

--- toffice.webapp-react/src/shared-components/NoData.js
+++ toffice.webapp-react/src/shared-components/NoData.js
@@ -5,8 +5,8 @@ import * as React from 'react';
 function NoData() {
 	return (
 		<div className="py-4">
-			<div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
-				{/* <img src="img/no-data.png" alt="No data" /> */}
+			<div className="flex items-center justify-center border-4 border-dashed border-gray-200 rounded-lg h-96">
+				<img src="img/no-data-sad-face.png" alt="No data" />
 			</div>
 		</div>
 	);

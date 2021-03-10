8ed3b92f8c60ef221f7da2937bb048f7a1693bbf
Darko Milutinovic <dmilutinovic@gmail.com>
2021-03-10 16:57:53 +0100

    react block ui

--- toffice.webapp-react/src/shared-components/TailwindTable.js
+++ toffice.webapp-react/src/shared-components/TailwindTable.js
@@ -1,10 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
+import BlockUi from 'react-block-ui';
 import {useTable, usePagination} from 'react-table';
 import NoData from './NoData';
 import TailwindTablePagination from './TailwindTablePagination';
 
-function TailwindTable({columns, data, total}) {
+import 'react-block-ui/style.css';
+
+function TailwindTable({columns, data, total, isDataLoading}) {
 	const {
 		getTableProps,
 		getTableBodyProps,
@@ -54,56 +57,58 @@ function TailwindTable({columns, data, total}) {
 				canNextPage={canNextPage}
 			/>
 
-			<div className="flex flex-col">
-				<div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-					<div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-						<div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-							<table
-								{...getTableProps()}
-								className="min-w-full divide-y divide-gray-200"
-							>
-								<thead className="bg-gray-50">
-									<tr>
-										{headers.map(column => (
-											<th
-												{...column.getHeaderProps()}
-												scope="col"
-												className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-											>
-												{column.render('Header')}
-											</th>
-										))}
-									</tr>
-								</thead>
-								<tbody {...getTableBodyProps()}>
-									{page.map((row, i) => {
-										prepareRow(row);
-										return (
-											<tr
-												{...row.getRowProps()}
-												className={`hover:bg-gray-100 ${
-													row.index % 2 === 0 ? 'bg-white' : 'bg-gray-50'
-												}`}
-											>
-												{row.cells.map(cell => {
-													return (
-														<td
-															{...cell.getCellProps()}
-															className="px-6 py-4 whitespace-nowrap text-sm font-medium"
-														>
-															{cell.render('Cell')}
-														</td>
-													);
-												})}
-											</tr>
-										);
-									})}
-								</tbody>
-							</table>
+			<BlockUi tag="div" blocking={isDataLoading}>
+				<div className="flex flex-col">
+					<div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+						<div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+							<div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+								<table
+									{...getTableProps()}
+									className="min-w-full divide-y divide-gray-200"
+								>
+									<thead className="bg-gray-50">
+										<tr>
+											{headers.map(column => (
+												<th
+													{...column.getHeaderProps()}
+													scope="col"
+													className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+												>
+													{column.render('Header')}
+												</th>
+											))}
+										</tr>
+									</thead>
+									<tbody {...getTableBodyProps()}>
+										{page.map((row, i) => {
+											prepareRow(row);
+											return (
+												<tr
+													{...row.getRowProps()}
+													className={`hover:bg-gray-100 ${
+														row.index % 2 === 0 ? 'bg-white' : 'bg-gray-50'
+													}`}
+												>
+													{row.cells.map(cell => {
+														return (
+															<td
+																{...cell.getCellProps()}
+																className="px-6 py-4 whitespace-nowrap text-sm font-medium"
+															>
+																{cell.render('Cell')}
+															</td>
+														);
+													})}
+												</tr>
+											);
+										})}
+									</tbody>
+								</table>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</BlockUi>
 
 			<div className="mt-5">
 				<TailwindTablePagination
